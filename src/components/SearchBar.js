5f0f6c3a9605270503ee2e429b520a37af3fd06a
Raphael Pacheco <raphaelrogen@gmail.com>
2022-10-25 18:24:06 -0300

    [Requisito 11] - Concluido

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import AppContext from '../context/AppContext';
 
 export default function SearchBar() {
+  const {
+    getFoodByIngredient,
+    getFoodByName,
+    getFoodByFirstLetter,
+  } = useContext(AppContext);
+  const [option, setOption] = useState('ingredient');
+  const [search, setSearch] = useState('');
+
+  const handleSearch = () => {
+    if (option === 'ingredient') {
+      getFoodByIngredient(search);
+    }
+
+    if (option === 'name') {
+      getFoodByName(search);
+      console.log('entrou em name');
+    }
+
+    if (option === 'firstLetter') {
+      if (search.length === 1) {
+        getFoodByFirstLetter(search);
+      } else {
+        global.alert('Your search must have only 1 (one) character');
+      }
+    }
+  };
+
+  const searchValue = ({ target }) => {
+    setSearch(target.value);
+  };
+
   return (
     <div>
       <input
         type="text"
         placeholder="pesquisar"
+        value={ search }
+        onChange={ searchValue }
         data-testid="search-input"
       />
 
@@ -14,6 +48,7 @@ export default function SearchBar() {
         <input
           type="radio"
           id="ingredientSearch"
+          onClick={ () => setOption('ingredient') }
           name="filter"
           data-testid="ingredient-search-radio"
         />
@@ -24,6 +59,7 @@ export default function SearchBar() {
         <input
           type="radio"
           id="nameSearch"
+          onClick={ () => setOption('name') }
           name="filter"
           data-testid="name-search-radio"
         />
@@ -34,12 +70,20 @@ export default function SearchBar() {
         <input
           type="radio"
           id="firstLetterSearch"
+          onClick={ () => setOption('firstLetter') }
           name="filter"
           data-testid="first-letter-search-radio"
         />
       </label>
 
-      <button type="button" data-testid="exec-search-btn">Pesquisar</button>
+      <button
+        onClick={ handleSearch }
+        type="button"
+        data-testid="exec-search-btn"
+      >
+        Pesquisar
+
+      </button>
     </div>
   );
 }
