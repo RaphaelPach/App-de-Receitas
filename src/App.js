efedabd310ae94dadb6c5e8f1f6583d0a702f10c
Raphael Pacheco <raphaelrogen@gmail.com>
2022-10-25 15:26:05 -0300

    [Requisito 01] - Concluido

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
 import Meals from './components/Meals';
@@ -10,12 +10,10 @@ import Provider from './context/Provider';
 function App() {
   return (
     <Provider>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route path="/meals" component={ Meals } />
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/meals" component={ Meals } />
+      </Switch>
     </Provider>
   );
 }
