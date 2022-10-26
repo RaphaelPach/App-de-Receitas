1d4af0fdf44671ee11ab71c46ba41d88af9c502c
Gabriel Batista <g.batiista@gmail.com>
2022-10-26 15:53:22 -0300

    [REQUISITO 14] - Concluido

--- src/components/Drinks.js
+++ src/components/Drinks.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
 import Header from './Header';
+import ItemCard from './ItemCard';
 
 export default function Drinks(props) {
+  const { drinkList } = useContext(AppContext);
+  const MAX_ITENS = 12;
+
   return (
     <div>
       <Header { ...props } title="Drinks" profile search />
+
+      {
+        drinkList?.filter((_, i) => i < MAX_ITENS).map((e, index) => (<ItemCard
+          key={ e.idDrink }
+          index={ index }
+          name={ e.strDrink }
+          img={ e.strDrinkThumb }
+        />))
+      }
     </div>
   );
 }
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
 import Header from './Header';
+import ItemCard from './ItemCard';
 
 function Recipes(props) {
+  const { foodList } = useContext(AppContext);
+  const MAX_ITENS = 12;
+
   return (
-    <Header { ...props } title="Meals" search profile />
+    <div>
+      <Header { ...props } title="Meals" search profile />
+      {
+        foodList?.filter((_, i) => i < MAX_ITENS).map((e, index) => (<ItemCard
+          key={ e.idMeal }
+          index={ index }
+          name={ e.strMeal }
+          img={ e.strMealThumb }
+        />))
+      }
+    </div>
   );
 }
 
