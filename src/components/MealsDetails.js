67626f03162385b2960d4145ed70143bc2f73033
Raphael Pacheco <raphaelrogen@gmail.com>
2022-10-28 18:38:12 -0300

    [Requisito 25] - Concluido

--- src/components/MealsDetails.js
+++ src/components/MealsDetails.js
@@ -5,6 +5,8 @@ export default function MealsDetails(props) {
   const { match: { params: { id } } } = props;
 
   const [data, setData] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+  const [videoUrl, setVideoUrl] = useState('');
 
   useEffect(() => {
     const fetchId = async (itemId) => {
@@ -13,12 +15,32 @@ export default function MealsDetails(props) {
       const { meals } = await response.json();
 
       setData(meals[0]);
+      setIngredients(Object.keys(meals[0]).filter((e) => e.includes('strIng')));
+
+      const embedVideo = meals[0].strYoutube.replace('watch?v=', 'embed/');
+      setVideoUrl(embedVideo);
     };
 
     fetchId(id);
   }, [id]);
 
-  return <div>{ data.idMeals }</div>;
+  return (
+    <div>
+      <h1 data-testid="recipe-title">{ data.strMeal }</h1>
+      <p data-testid="recipe-category">{ data.strCategory }</p>
+      <img data-testid="recipe-photo" src={ data.strMealThumb } alt={ data.strMeal } />
+      <p data-testid="instructions">{ data.strInstructions }</p>
+      {
+        ingredients?.map((e, i) => (
+          <div data-testid={ `${i}-ingredient-name-and-measure` } key={ e }>
+            <p>{data[e]}</p>
+            <p>{ data[`strMeasure${i + 1}`] }</p>
+          </div>
+        ))
+      }
+      <iframe data-testid="video" src={ videoUrl } title="Recipe Video" />
+    </div>
+  );
 }
 
 MealsDetails.propTypes = {
