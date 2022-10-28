acb2022c43d70ed315dc09776daf6255660abc5b
Raphael Pacheco <raphaelrogen@gmail.com>
2022-10-28 17:36:10 -0300

    [Requisito 24] Concluido

--- src/components/DrinksDetails.js
+++ src/components/DrinksDetails.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-export default function DetailsDrinks() {
-  return <div>DetailsDrinks</div>;
+export default function DetailsDrinks(props) {
+  const { match: { params: { id } } } = props;
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchId = async (itemId) => {
+      const URL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${itemId}`;
+      const response = await fetch(URL);
+      const { drinks } = await response.json();
+
+      setData(drinks[0]);
+    };
+
+    fetchId(id);
+  }, [id]);
+
+  return <div>{ data.idDrink }</div>;
 }
+
+DetailsDrinks.propTypes = {
+  match: PropTypes.shape().isRequired,
+};
--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -15,11 +15,32 @@ describe('Testando App de Receitas', () => {
     const chickenBtn = getByTestId('Chicken-category-filter');
     const dessertBtn = getByTestId('Dessert-category-filte');
     const goatBtn = getByTestId('Goat-category-filter');
+    const all = getByTestId('All-category-filter');
 
     expect(beefBtn).toBeInTheDocument();
     expect(breakfastBtn).toBeInTheDocument();
     expect(chickenBtn).toBeInTheDocument();
     expect(dessertBtn).toBeInTheDocument();
     expect(goatBtn).toBeInTheDocument();
+    expect(all).toBeInTheDocument();
+  });
+  it('cobrir 90%', async () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/meals');
+    });
+    const ordinaryBtn = getByTestId('Ordinary Drink-category-filter');
+    const cocktail = getByTestId('Cocktail-category-filter');
+    const shake = getByTestId('Shake-category-filter');
+    const unknown = getByTestId('Other/Unknown-category-filter');
+    const cocoa = getByTestId('Cocoa-category-filter');
+    const all = getByTestId('All-category-filter');
+
+    expect(ordinaryBtn).toBeInTheDocument();
+    expect(cocktail).toBeInTheDocument();
+    expect(shake).toBeInTheDocument();
+    expect(unknown).toBeInTheDocument();
+    expect(cocoa).toBeInTheDocument();
+    expect(all).toBeInTheDocument();
   });
 });
