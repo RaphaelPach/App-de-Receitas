f5d7f77312bb8aed747158f794e57be23368d7ea
Állex <allexwithx@gmail.com>
2022-10-28 14:14:59 -0300

    Requisito 19

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,22 +1,39 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import AppContext from '../context/AppContext';
 import Header from './Header';
 import ItemCard from './ItemCard';
 
 function Recipes(props) {
+  const [getFood, setGetFood] = useState([]);
+
   const { foodList } = useContext(AppContext);
   const MAX_ITENS = 12;
+
+  const renderFoodCards = (data) => data
+    .filter((_, i) => i < MAX_ITENS)
+    .map((e, index) => (<ItemCard
+      key={ e.idMeal }
+      index={ index }
+      name={ e.strMeal }
+      img={ e.strMealThumb }
+    />));
+
+  useEffect(() => {
+    const getFoodList = async () => {
+      const responseName = await fetch(
+        'https://www.themealdb.com/api/json/v1/1/search.php?s=',
+      );
+      const { meals } = await responseName.json();
+      setGetFood(renderFoodCards(meals));
+    };
+    getFoodList();
+  }, []);
 
   return (
     <div>
       <Header { ...props } title="Meals" search profile />
       {
-        foodList?.filter((_, i) => i < MAX_ITENS).map((e, index) => (<ItemCard
-          key={ e.idMeal }
-          index={ index }
-          name={ e.strMeal }
-          img={ e.strMealThumb }
-        />))
+        foodList.length > 0 ? renderFoodCards(foodList) : getFood
       }
     </div>
   );
