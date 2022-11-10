3cde55254a05e12330f41d593dd8d31bf25d3390
Raphael Pacheco <raphaelrogen@gmail.com>
2022-11-10 05:03:36 -0300

    searchbar

--- src/tests/searchBar.test.js
+++ src/tests/searchBar.test.js
@@ -1,4 +1,4 @@
-/* import React from 'react';
+import React from 'react';
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
@@ -144,4 +144,3 @@ describe('Testando SearchBar', () => {
     });
   });
 });
- */
